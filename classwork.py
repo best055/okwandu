9dbab23a6e3b1479f09290a3b0a07209c52a2600
best055 <donbest055@gmail.com>
2025-03-13 15:20:23 -0700

    This is my first commit

---
+++ classwork.py
@@ -0,0 +1,481 @@
+# # creating veriable
+# # x = 5
+# # y = 'sunji bright'
+# # z = '23'
+# # # output
+# # print(x)
+# # print(y)
+# # print(z)
+
+
+# # new_list = list((2, 56, 37))
+# # print(new_list) 
+
+# # my_list =['cherry','banana', 56,7.3]
+# # print(my_list[1])
+
+
+# # this_list = ["apple","banana","cherry","orange","kiwi","melon","mango","sweet","lemon","press","bright","sunji"]
+# # print(this_list[1:9:2])
+# # new_list = list(this_list[0:10:2])
+# # print(this_list)
+# # print(new_list)
+# # print(this_list[-7:-1])
+# # if "banana" in this_list:
+# #     print("yes, 'banana' is in the fruit list")
+# # this_list[1] = "okwandu"
+# # print(this_list)
+# # this_list[1:3] = ["okwandu", "watermelon", "itel"]
+# # print(this_list)    
+
+
+# # qus = input('enter your input')
+# # first_qus:
+
+# # for i in qus:
+# #     if i.isalpha():
+# #         first_qus += i
+
+# # new_qus = ''
+# # for i in renge(len(first_qus)):
+# #     if i % 2 == 0:
+# #         new_qus += first_qus[i].upper
+# #     else
+# #         new_qus += first_qus[i].lower()
+# # print(new_qus)                
+
+
+
+# # my_list = [1, 5, 2, 5, 3, 5, 4]
+# # new_list = list(set(my_list))
+# # print(new_list)
+
+
+# # my_var = input('enter your string: ')
+# # my_var = my_var.lower().replace(' ', '')
+# # if my_var == my_var[::-1]:
+# #     print('it is palindrome')
+# # else:
+# #     print('it is not')    
+
+
+
+# # y_tup = (2, 43, 67)
+# # print(tympe(my_tup))
+# # my_tup.
+
+# # thistuple = ("apple",)
+# # print(type(thistuple))
+
+# # #not a tuple
+# # thistuple = ("apple")
+# # print(type(thistuple))
+
+# # x = ("apple", "banana", "cherry")
+# # y = list(x)
+# # y[1] = "sunji"
+# # x = tuple(y)
+
+# # print(x)
+
+
+
+# # fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+# # (green, yellow, *red) =fruits
+
+# # print(green)
+# # print(yellow)
+# # print(red)
+
+
+# # thisset = {"apple", "banana", "cherry" "apple", "orange", "cherry"}
+# # print(thisset)
+
+# # thisset = {"apple", "banana", "cherry", 1, 2}
+
+# # print(thisset)
+
+# # thisset = {"apple", "banana", "cherry"}
+# # # tropical = {"pineapple", "mango", "papaya"}
+# # thisset.remove("banana")
+# # print(thisset)
+
+# # thisdict = {"brand": "ford", "model": "mustang", "year": 1964}
+
+# # print(thisdict["brand"])
+
+
+# # thisdict = dict(name = "john", age = 67, country = "norway")
+# # print(thisdict)
+
+
+# # first_list = ["name", "age", "email"]
+# # second_list = ["bright", 67, "bright@gmail.com"]
+
+# # mydict = dict(zip(first_list, second_list))
+# # print(mydict)
+
+# # x = 5
+# # x += 6
+# # x -= 1
+# # x *= 7
+
+# # x = 5
+# # y = 10
+# # print(x == y)
+# # print(x >= y)
+# # print(x <=y)
+# # print(x != y)
+# # print(x > y)
+
+
+# # # # CONDITIONAL STATEMENTS
+# # name = input("enter name: ")
+# # program = input("enter your program(techrise/stream B): ")
+# # if "techrise" == program:
+# #     print("hello", name)
+# # elif "stream B" == program:
+# #     print("hello", name)    
+# # else:
+# #     print(f"we do not recognize {name} in the class")    
+
+
+
+
+# # list1 = []
+# # list2 = []
+
+# # while true:
+# #     first_input = input('enter a string for the first list: ')
+# #     first_input = first_input.strip()
+# #     list1.append(first_input)
+# #     qus = input('are you through with the first list? y/n: ')
+# #     if qus == 'y':
+# #         break
+
+# #         while true:
+# #             second_input = input('enter a string for the second list: ')
+# #             second_input = second_input.strip()
+# #             list2.append(second_input)
+# #             qus1 = input("are you through with the second list2? (yes/no) ")
+# #             if qus1 == "yes":
+# #                 break
+
+# # if len(list1) != len(list2):
+# #     print('the lenght of the two list are not the same')
+# # else
+# #     my_dict = dict(zip(list1, list2))
+# #     print(my_dict)
+
+
+
+# # print(15//2)
+
+# # x = 25
+# # x+=3 
+# # print(x)
+# # x-=6
+# # print(x)
+
+# # print(7 & 5)
+
+# # print(3 << 2)
+
+# # print(5 + 4 - 7 + 3)
+
+
+# # a = int(input('enter your number: '))
+# # b = int(input('enter the second number: '))
+
+# # if b > a:
+# #     print(b-a)
+# #     print("b is greater than a")
+# # elif a > b:
+# #     print(a-b)
+# #     print('a is greater than b')
+# # else:
+# #     print('the two numbers are the same')    
+
+
+# # QUS 1
+
+# # number = int(input("enter a number "))
+
+# # # check if the year is a leap year
+# # if number % 2 == 0 and(number % 100 != 0 or number % 400 == 0):
+# #     print(f"{number} is an even number")
+# # else:
+# #     print(f"{number} is not an even number")   
+
+
+
+# # create a program thtat asks users for their age and classifiles them into age groups
+# # child: 0-12
+# # teen: 13-19
+# # adult: 20-64
+# # senior: 65+
+
+
+# # age = int(input("what is your age? "))
+# # if age <= 12:
+# #     print("you are a child")
+# # elif age >=13 and age <=19:
+# #     print("you are a teen")
+# # elif age >= 20 and age <= 64:
+# #     print("you are an adult")
+# # elif age >= 64:
+# #     print("you are a senior")
+
+
+# # str = "WESTAFRICA"
+# # mylist = ["solace", "bright", "cletus", "merit"]
+
+# # for i in mylist:
+# #     print(i)
+# # print(i)    
+
+
+# # # num = [1, 2, 3, 4, 5]
+
+# # num = 10
+
+# # while num > 1:
+# #     num -= 1
+# #     print(num)
+
+
+# # SIR UCHE
+
+# # # write a code to each of the following is pailndrome
+# # "the quick brown dog jamp over the lazy dog"
+# # "madam"
+# # "never odd or even"
+# # "dennis and edna sinned"
+# # "obi is a boy"
+
+# # list = {"the quick brown dog jump over the lazy dog", "madam", "ever odd or even", "dennis and edna sinned", "obi is a boy"}
+
+# # for i in list:
+# #     if i [::-1].lower().replace(" ","") == i.lower().replace(" ",""):
+# #         print(f"{i} is a pailndrome")
+# #     else:
+# #         print(f"{i} is not a pailndrome")    
+
+
+# # name = input("enter your name: ")
+# # age = int(input("enter your age: "))
+# # gender = input('enter your gender: ')
+
+# # if age < 18 or age > 50:
+# #     print(f"{name} you are not qualified for this recruitment")
+# # else:
+# #     if (gender == 'male' and age < 25) or (gender == 'female' and age < 31):
+# #         print(f'{name} you will be in the customer care department')
+# #     elif gender == 'male' and age <= 45:
+# #         print(f'{name} you are placed in the engineering department')
+# #     elif gender == 'female' and age >= 31:
+# #         print(f'{name} you are placed in the admin department')
+# #     else:
+# #         print(f'{name} you are placed in the security department')
+
+
+
+# # i = 1
+# # while i < 6:
+# #     print(i, end='')
+# #     if (i == 3):
+# #         break
+# #     i += 1    
+
+# # i = 0
+# # while i < 6:
+# #   i += 1
+# #   if i ==3:
+# #     continue
+# # print(i)    
+
+
+# # fruits = ["apple", "banana", "chery"]
+# # for x in fruits:
+# #     print(x)
+
+# # adj = ["red", "big", "tasty"]
+# # fruits = ["apple", "banana", "cherry"]
+
+# # for x in adj:
+# #     for y in fruits:
+# #         print(x, y)
+
+# # for i in range(4):
+# #     for j in range(4):
+# #          print('#', end=' ')
+# #     print()  
+
+# # qus1
+
+# for i in range(2, 13):
+#      print(f'multiplication table of {i}: ')
+#      for j in range(1, 11):
+#           print(f'{i} x {j} = {i * j}')
+#      print()     
+
+# # qus2A
+
+# # def main():
+# #      year = int(input('enter a year: '))
+# #      print(f'you entered the year: {year}')
+
+# # if __name__ == "__main__":
+# #      main()     
+
+# # qus2B
+
+# def get_month(month_input):
+#      month_names = {1: 'january', 2: 'fabruary', 3: 'march', 4: 'april', 5: 'may', 6: 'june', 7: 'july', 8: 'august', 9: 'september', 10: 'october', 11: 'november', 12: 'december', }
+     
+#      if month_input.isdigit():
+#           month = int(month_input)
+#           if 1 <= month <= 12:
+#                return month
+#           else:
+#                print('invalid month number. please enter a number between 1 and 12.')
+#                return none                        
+
+# # qus2C
+
+# function to print the calendar of a specific month and year
+# def print_month_calendar(year, month):
+#     cal = calendar. textcalender(calender.sunday)
+
+#     month_calender = cal.formatmonth(year, month)
+#     print(month_calender)
+
+# year = 1998
+# month = 7
+# print_month_calender(year, month)    
+
+# LAST
+
+# def print_triangle(height):
+#     for i in range(2, height + 2):
+#         print('* ' * i)
+#         print_triangle(10)
+
+# n = 7
+# for i in range(n, 0, -1):
+#     for j in range(n-1):
+#         print(' ', end='')
+#     for k in range(i):
+#         print('# ', end='')
+#     print()        
+
+# n = 7 
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ", end =" ")
+#     for k in range(i):
+#         print(" * ", end =" ")
+#     print()
+# n-=1
+# for i in range(n, 0, -1):
+#     for j in range(n-i):
+#         print("   ", end =" ")
+#     for k in range(i):
+#         print(" * ", end =" ")
+#     print()                
+
+# def exponential(num, power):
+#     result = num ** power
+#     return result
+
+# print(exponential(10,2))        
+
+# def multiply(num1, num2, *num3):
+#     result = num1 * num2
+#     x = [*num3]
+#     for i in x:
+#         result = result * i
+#     return result    
+
+# print(multiply(3,7, 8, 90,8,7))   
+
+# def greet(fname, iname):
+#     return f"good afternoon {fname} {iname}"
+
+# print(greet("bright", "chibuzor"))
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if x != "apple"]
+
+# print(newlist)
+
+# newlist = [x for x in range(10) if x % == 3]
+# print(newlist)
+
+# newlist = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x.upper() for x in newlist if x != "mango"]
+
+# print(newlist)
+
+# thislist = ["orange", "mango", "kiwi", "pineaple", "banana"]
+
+# thislist.sort(reverse = True)
+
+# print(thislist)
+
+# myfamily = {
+#     "child1" : {
+#        "name" : "emil",
+#        "year" : 2004
+#     },
+
+#     "child2" : {
+#         "name" : "tobias",
+#         "year" : 2007
+# },
+
+#     "child3" : {
+#         "name" : "linus",
+#         "year" : 2011
+# }
+# }
+
+# for x, obj in myfamily.items():
+#     print(x)
+
+#     for y in obj:
+
+#         print(y + ':', obj[y])
+#     print()    
+
+
+# def my_function(fname):
+#     print(fname + "refsnes")
+
+# my_function("emil")
+# my_function("tobias")
+# my_function("linus")    
+
+
+# def my_function():
+#     print("hello from a function")
+
+# my_function()
+
+# def my_add(a, b):
+#     print(a/b)
+
+# my_add(3, 5)    
+
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence[:n]
+
+n = int(input("enter the number of fibonacci numbers to generate: "))
+print(fibonacci(n))        
